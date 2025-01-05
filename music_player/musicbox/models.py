92f2bcacbe8f5f14acabe348bf03df8ada2295d8
varshanixon <varshanixon17@gmail.com>
2025-01-05 23:59:31 +0530

    Created User model
    Created a custom user model with phone and dob as additional fields

--- music_player/musicbox/models.py
+++ music_player/musicbox/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
+
+class User(AbstractUser):
+
+    phone = models.CharField(max_length=15,unique=True,blank=False,null=False)
+
+    dob = models.DateField(blank=False,null=False)
 
