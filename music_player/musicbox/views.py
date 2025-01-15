685d9d5fd3489ea431bb35e612f7e4979f3714e5
varshanixon <varshanixon17@gmail.com>
2025-01-15 20:58:50 +0530

    Update views.py

--- music_player/musicbox/views.py
+++ music_player/musicbox/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.views.generic import View
+
+from musicbox.forms import SignUpForm
+
 # Create your views here.
+
+
+class SignUpView(View):
+
+    template_name = 'signup.html'
+
+    form_class = SignUpForm
+
+    def get(self,request,*args,**kwargs):
+
+        form_instance = self.form_class
+
+        return render(request,self.template_name,{'form':form_instance})
