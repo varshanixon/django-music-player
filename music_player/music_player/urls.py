cafe0657522a31e7ea426cc4cdce28284f984e0a
varshanixon <varshanixon17@gmail.com>
2025-01-15 21:02:53 +0530

    Update urls.py

--- music_player/music_player/urls.py
+++ music_player/music_player/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from musicbox import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('signup/',views.SignUpView.as_view(),name='signup'),
 ]
