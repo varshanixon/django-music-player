78c8d7a6a34c173190b0ebf13f1ff7517c4eeba8
varshanixon <varshanixon17@gmail.com>
2025-01-15 20:54:14 +0530

    Create forms.py

---
+++ music_player/musicbox/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+
+from musicbox.models import User
+
+
+
+class SignUpForm(UserCreationForm):
+
+    class Meta:
+
+        model = User
+
+        fields = ['username','first_name','last_name','dob','phone','email','password1','password2']
