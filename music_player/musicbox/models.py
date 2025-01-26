31dc25101996d4923d4e1dba29a1fadf108ad258
varshanixon <varshanixon17@gmail.com>
2025-01-27 00:29:27 +0530

    modified User model
    modified User model
    applied migrations

--- music_player/musicbox/models.py
+++ music_player/musicbox/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
+from random import randint
+
 # Create your models here.
 
 # Custom User Model
@@ -11,7 +13,16 @@ class User(AbstractUser):
 
     dob = models.DateField(blank=True,null=True)
 
+    is_verified = models.BooleanField(default=False)
 
+    otp = models.CharField(max_length=15,null=True)
+
+    def get_otp(self):
+
+        self.otp = str(randint(1000,9999))
+
+        self.save()
+    
     def __str__(self):
 
         return self.username
